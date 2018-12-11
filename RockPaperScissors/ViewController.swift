7bf8ff473edf4b5f5c892659e26e57a39b0d0e00
NATHAN GEHRKE <rl107106@rlas-macair-22.local>
2018-12-11 11:21:52 -0600

    Initial Commit

---
+++ RockPaperScissors/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by NATHAN GEHRKE on 12/3/18.
+//  Copyright © 2018 Nathan Gehrke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+//The AI would randomly pick either variables of Rock, Paper, or Scissors. The user picks one of the variables, and the AI picks one randomly. Then, the variables are compared in an if statement to see which variable is greater than the other, and would decide the victor accordingly.
+    
+    //Rock = 
+
+}
+
+
